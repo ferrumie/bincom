ea37b3526e146222b7a179ca7a430284a2992515
feranmi <alabiemmanuelferanmi@lgmail.com>
2021-07-15 21:28:51 +0100

    creating table

--- colours.py
+++ colours.py
@@ -1,7 +1,16 @@
 import re
 from statistics import median
+from math import factorial
+import operator
+from functools import reduce
+import psycopg2
 
-
+connection = psycopg2.connect(
+    host="localhost",
+    database="bincom",
+    user="postgres",
+    password=None,
+)
 def list_to_dict(list):
     """
     To convert a list to a dictionary with the even elements mapped to the odd elements
@@ -74,4 +83,23 @@ median_numb = list(colour_count.keys())[list(
 print(f"median colour is the {median_numb} colour")
 
 
-# Task 4
+# Task 4 variance
+# we need the mean
+var_res = sum((xi - mean_val) ** 2 for xi in li) / len(li)
+mean_val = var_res
+closest_value = min(values, key=absolute_difference_function)
+variance_col = list(colour_count.keys())[list(
+    colour_count.values()).index(closest_value)]
+print(f"median colour is the {variance_col} colour")
+
+# task 5, probability of picking a red
+# We can use combinatorics for this
+# We need the number of red , and the total colour
+
+# total number of red
+red_num = colour_count.get("RED")
+
+# probability of picking a red colour is 
+prob = red_num/total
+print(f"probability of picking red is {prob*100} percent")
+
